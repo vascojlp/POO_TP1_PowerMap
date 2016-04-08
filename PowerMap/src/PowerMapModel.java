6a881f73feba4d2779b96db08c40920b62e5e7b5
Vascojlp@gmail.com <vascojlp@gmail.com>
2016-04-08 14:30:18 +0100

    vasco 8-4-2016

---
+++ PowerMap/src/PowerMapModel.java
@@ -0,0 +1,28 @@
+import java.io.FileReader;
+
+/**
+ * Created by Vasco on 08/04/2016.
+ */
+public class PowerMapModel {
+
+    public int getNumLines() {
+        return 0;
+    }
+
+    public int getNumColumns() {
+        return 0;
+    }
+
+    public void setChangeListener(PowerMapView view) {
+    }
+
+    public boolean isCompleted() {
+        return false;
+    }
+
+    public void rotate(int lin, int col) {
+    }
+
+    public void loadLevel(FileReader file) {
+    }
+}
---
+++ PowerMap/src/PowerMapView.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Vasco on 08/04/2016.
+ */
+public class PowerMapView {
+    public static final int CELL_HEIGHT = 50;
+    public static final int CELL_WIDTH = 50;
+
+    public void setViews(PowerMapModel model) {
+    }
+
+    public void repaintAll(PowerMapModel model) {
+    }
+}
